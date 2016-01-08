10027d37d9700c99a47362c20ec8dac73885ce11
Sergey Romanov <xxsmotur@gmail.com>
2016-01-08 23:55:09 +0500

    Add comments

--- schema.go
+++ schema.go
@@ -1,15 +1,18 @@
 package magneticdb
 
 
+// Schema provides default definition of schema
 type Schema struct {
 	Tables []*Table
 }
 
+// Table must contain name and list of clumns
 type Table struct {
 	Name string
 	Columns []*Column
 }
 
+// Column must contain name
 type Column struct {
 	Name  string
 }
