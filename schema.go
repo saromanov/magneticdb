12305d07893d8c6fb980c5ea5ec8e8ee307f2fd1
Sergey Romanov <xxsmotur@gmail.com>
2016-01-09 05:01:45 +0500

    Starts to define Validate schema

--- schema.go
+++ schema.go
@@ -1,5 +1,9 @@
 package magneticdb
 
+import(
+   "reflect"
+   "fmt"
+)
 
 // Schema provides default definition of schema
 type Schema struct {
@@ -15,4 +19,15 @@ type Table struct {
 // Column must contain name
 type Column struct {
 	Name  string
+}
+
+// ValidateSchema provides validation of schema and return true
+// if schema is valid and false otherwise
+func ValidateSchema(schema *Schema)bool {
+	start := reflect.ValueOf(schema).MapKeys()
+	fmt.Println(start)
+	/*for _, item := range start {
+		fmt.Println(reflect.TypeOf(item).Name())
+	}*/
+	return true
 }
